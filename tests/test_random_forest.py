b25777fec04557edaecd369171fde277023ec8e8
Roberto <roberto@MacBook-Pro-de-Roberto.local>
2021-12-30 10:45:30 +0100

    RF test added

--- tests/test_random_forest.py
+++ tests/test_random_forest.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from ml.datasets.blobs import Blobs
 from ml.models.random_forest import RandomForest
+from ml.models.decision_tree import DecisionTree
 from ml.metrics.classification import ClassificationMetrics
 
 
@@ -16,33 +17,25 @@ def get_data():
 def test_init():
     X, y = get_data()
 
-    dec_tree = DecisionTree(is_classification=True)
-    dec_tree.fit(X, y)
+    rf = RandomForest(n_trees=3, max_depth=3, batch_size=30, is_classification=True)
+    rf.fit(X, y)
 
     # Assert not None root and first splits
-    assert dec_tree.root is not None
-    assert dec_tree.root.left is not None
-    assert dec_tree.root.right is not None
-
-    # Assert deeps
-    assert dec_tree.root.deep == 0
-    assert dec_tree.root.left.deep == 1
-
-    # Assert leaf node
-    cond1 = dec_tree.root.left.left.split_point["class_left"]
-    cond2 = dec_tree.root.left.left.left
-    assert cond1 == 0 and cond2 is None
+    assert len(rf.forest) is 3
+    # Aseert forest are DecisionTree
+    for tree in rf.forest:
+        assert isinstance(tree, DecisionTree)
 
 
 def test_depth_hyperparameter():
     X, y = get_data()
 
-    dec_tree = DecisionTree(is_classification=True)
-    dec_tree.fit(X, y)
-    acc_overfit = ClassificationMetrics(y, dec_tree.predict(X)).get_accuracy()
+    rf = RandomForest(n_trees=3, max_depth=10, batch_size=30, is_classification=True)
+    rf.fit(X, y)
+    acc_overfit = ClassificationMetrics(y, rf.predict(X)).get_accuracy()
 
-    dec_tree = DecisionTree(is_classification=True, max_depth=2)
-    dec_tree.fit(X, y)
-    acc_underfit = ClassificationMetrics(y, dec_tree.predict(X)).get_accuracy()
+    rf = RandomForest(n_trees=1, max_depth=1, batch_size=30, is_classification=True)
+    rf.fit(X, y)
+    acc_underfit = ClassificationMetrics(y, rf.predict(X)).get_accuracy()
 
     assert acc_overfit > acc_underfit
