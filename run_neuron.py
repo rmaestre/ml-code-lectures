4020e87a8d752f29835a88dbdded13f5ae7b26c2
Roberto <roberto@MacBook-Pro-de-Roberto.local>
2021-12-30 12:03:56 +0100

    Add accuracy metrics to neuron runner

--- run_neuron.py
+++ run_neuron.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from ml.datasets.blobs import Blobs
 from ml.models.neuron import Neuron
 
+from ml.metrics.classification import ClassificationMetrics
+from sklearn.metrics import confusion_matrix, classification_report
+
 
 # Generate synthetic dataset
 dataset = Blobs()
@@ -20,6 +23,14 @@ print(neuron.weights)
 # Predict labels
 y_hats = neuron.predict(X)
 print(y_hats)
+print(y_hats > 0.5)
+
+# get accuracy
+metrics = ClassificationMetrics(y, y_hats > 0.5)
+print("Accuracy %f %%" % metrics.get_accuracy())
+print("Confusion Matrix %f")
+print(confusion_matrix(y, y_hats > 0.5))
+print(classification_report(y_hats > 0.5, y))
 
 # Plot
 for px, py, lab, m in zip(X[:, 0], X[:, 1], y == 1.0, y_hats > 0.5):
